96edf861baa3906a7d5cb878fbdb9073ee7ea129
Richard <richard.williams2@man.ac.uk>
2015-02-04 09:29:30 +0000

    modifications

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ var bb = {};
 var getPie = function (data, colours, element, onclick) {
 	var pie = {
 		bindto: element,
-        color: {
-            pattern: colours
-        },
+        color: { pattern: colours },
         tooltip: {
             format: {
                 value: function (value, ratio, id, index) {
@@ -19,9 +17,7 @@ var getPie = function (data, colours, element, onclick) {
         data: {
             columns: data,
             type: 'pie',
-			selection: {
-				enabled: true
-			}
+			selection: { enabled: true }
         },
 		pie: {
 			label: {
@@ -31,9 +27,7 @@ var getPie = function (data, colours, element, onclick) {
 			}
 		}
 	};
-	if(onclick){
-		pie.data.onclick = onclick;
-	}
+	if(onclick){ pie.data.onclick = onclick; }
 	return pie;
 };
 
@@ -44,23 +38,29 @@ var populateSuggestedActions = function (id){
 	$('#sap').html(rendered);
 };
 
+var populatePatients = function (id) {
+	var template = $('#patient-list').html();
+	Mustache.parse(template);   // optional, speeds up future uses
+	var rendered = Mustache.render(template, bb.data.items[id]);
+	$('#patients').html(rendered);
+}
+
+var selectPieSlice = function (chart){
+	bb.chartClicked=true;
+	$('#' + chart + ' path.c3-arc').attr('class', function(index, classNames) {
+		return classNames + ' _unselected_';
+	});
+	bb[chart].unselect();
+	bb[chart].select(d.id);
+};
+
 var showOverviewCharts = function () {
     bb.chart1 = c3.generate(getPie(bb.data.unmeasured, ['#845fc8', '#a586de', '#6841b0'], '#chart1', function(d,i){
-		bb.chartClicked=true;
-		$('#chart1 path.c3-arc').attr('class', function(index, classNames) {
-			return classNames + ' _unselected_';
-		});
-		bb.chart1.unselect();
-		bb.chart1.select(d.id);
+		selectPieSlice('chart1');
 		populateSuggestedActions(d.id);
 	}));
 	bb.chart2 = c3.generate(getPie(bb.data.main, ['#845fc8', '#f96876'], '#chart2', function (d, i) {
-		bb.chartClicked=true;
-		$('#chart2 path.c3-arc').attr('class', function(index, classNames) {
-			return classNames + ' _unselected_';
-		});
-		bb.chart2.unselect();
-		bb.chart2.select(d.id);
+		selectPieSlice('chart2');
 		if (d.id === "Unmeasured") {
 			$('#chart1').show(400);
 			$('#chart3').hide(400);
@@ -80,12 +80,7 @@ var showOverviewCharts = function () {
 		}
 	} ));
 	bb.chart3 = c3.generate(getPie(bb.data.uncontrolled, ['#f96876', '#fc8d97', '#f6495a'], '#chart3', function(d,i){
-		bb.chartClicked=true;
-		$('#chart3 path.c3-arc').attr('class', function(index, classNames) {
-			return classNames + ' _unselected_';
-		});
-		bb.chart3.unselect();
-		bb.chart3.select(d.id);
+		selectPieSlice('chart3');
 		populateSuggestedActions(d.id);
 	}));
 
@@ -103,20 +98,14 @@ var show = function (page) {
 };
 
 var wireUpPages = function () {
-
-    // wire up site
     show('login');
 
     $('#navbar').on('click', 'a', function () {
         $(".nav").find(".active").removeClass("active");
         $(this).parent().addClass("active");
-        if (this.href.split('#')[1] === 'about') {
-            show('page2');
-        } else if (this.href.split('#')[1] === 'contact') {
-            show('page3');
-        } else {
-            show('login');
-        }
+        if (this.href.split('#')[1] === 'about') { show('page2'); } 
+		else if (this.href.split('#')[1] === 'contact') { show('page3'); } 
+		else { show('login'); }
     });
     $('#login-button,#create-button,#forgot-button,#home-button').on('click', function (e) {
         show('page1');
@@ -128,11 +117,7 @@ var wireUpPages = function () {
 
 var loadData = function() {
 	$.getJSON("data.json", function(data) {
-		bb.data = {};
-		bb.data.all = data;
-		bb.data.items = {};
-		bb.data.unmeasured = [];
-		bb.data.uncontrolled = [];
+		bb.data = {"all" : data, "items" : {}, "unmeasured" : [], "uncontrolled": []};
 		bb.data.main = [['Unmeasured', bb.data.all.unmeasured.n],['Uncontrolled', bb.data.all.uncontrolled.n]];
 		for(var i=0; i < bb.data.all.unmeasured.items.length; i++) {
 			bb.data.unmeasured.push([bb.data.all.unmeasured.items[i].name, bb.data.all.unmeasured.items[i].n]);
