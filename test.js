98c5f621870ebe8f241ed5bb4d86dcee88820aeb
Richard <richard.williams2@man.ac.uk>
2015-07-09 15:25:37 +0100

    Changes

--- test.js
+++ test.js
@@ -1,15 +1,24 @@
-var webd = require('webdriverio');
-var options = {
-    desiredCapabilities: {
-        browserName: 'firefox'
-    }
-};
+var webdriver = require('selenium-webdriver'),
+    SeleniumServer = require('selenium-webdriver/remote').SeleniumServer;
 
-webd
-    .remote(options)
-    .init()
-    .url('http://www.google.com')
-    .title(function(err, res) {
-        console.log('Title was: ' + res.value);
-    })
-    .end();
+    var server = new SeleniumServer('./node_modules/selenium-standalone/.selenium/selenium-server/2.46.0-server.jar', {
+          port: 4444
+    });
+
+    server.start();
+
+    var driver = new webdriver.Builder().
+        usingServer(server.address()).
+            withCapabilities(webdriver.Capabilities.firefox()).
+                build();
+
+                driver.get('http://www.google.com');
+                driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
+                driver.findElement(webdriver.By.name('btnG')).click();
+                driver.wait(function() {
+                     return driver.getTitle().then(function(title) {
+                            return title === 'webdriver - Google Search';
+                             });
+                }, 1000);
+
+                driver.quit();
