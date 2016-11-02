8e319922096c1d9245057c8812f7b546de3125b9
Richard Williams <richard.williams2@manchester.ac.uk>
2016-11-02 15:38:20 +0000

    event logging started to add

---
+++ server/controllers/events.js
@@ -0,0 +1,58 @@
+var Event = require('../models/event');
+
+var e = {
+
+  //Get list of events for a given type - or all
+  list: function(filter, skip, limit, done) {
+    Event.find(filter, null, {skip: +skip || 0, limit: +limit || 100, sort: {date: -1}}, function(err, events) {
+      if (err) {
+        console.log(err);
+        return done(new Error("Error finding event list"));
+      }
+      if (!events) {
+        console.log('Error finding event list');
+        return done(null, false);
+      } else {
+        done(null, events);
+      }
+    });
+  },
+
+  add: function(event, done){
+    if (event) {
+      //record to mongo
+      try {
+        var newEvent = new Event(event);
+
+        // save the event
+        newEvent.save(function(err,evt) {
+          if (err) {
+            return done(err);
+          } else {
+            console.log(evt);
+            return done(null);
+          }
+        });
+      } catch(e) {
+        return done(e);
+      }
+    }
+    else {
+      return done("oops");
+    }
+  },
+
+  login: function(email, session) {
+    var newEvent = new Event({sessionId:session, user: email, type: "login"});
+
+    // save the event
+    newEvent.save(function(err) {
+      if (err) {
+        console.log("Error writing login event: " + err);
+      }
+    });
+  }
+
+};
+
+module.exports = e;
