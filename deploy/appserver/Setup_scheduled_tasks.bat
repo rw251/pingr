d92481d069b30554139ab3039bb3086dd725beb9
Richard Williams <richard.williams2@manchester.ac.uk>
2016-11-06 21:41:14 +0000

    Backupof data daily

--- deploy/appserver/Setup_scheduled_tasks.bat
+++ deploy/appserver/Setup_scheduled_tasks.bat
@@ -4,6 +4,8 @@ REM ================================
 
 SET BATCH.DIRECTORY=D:\pingr\deploy\appserver\
 SET START.BAT=D:\pingr\start.bat
+SET BACKUP.BAT=D:\pingr\deploy\appserver\mongobackup.bat
 
 SchTasks /RU system /Create /SC DAILY /TN "PINGR Data Loader" /TR "\"%BATCH.DIRECTORY%RunPINGRLoaderScript_ST_DEBUG.bat\"" /RI 20 /ST 00:12 /DU 23:00
 SchTasks /RU system /Create /SC ONSTART /TN "PINGR web app" /TR "\"%START.BAT%\""
+SchTasks /RU system /Create /SC DAILY /TN "PINGR MongoDB backup" /TR "\"%BACKUP.BAT%\"" /ST 21:00
