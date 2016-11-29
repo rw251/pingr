7d955b1dc734279b8cd048dec42f281808b00912
Republicof1 <republicof1@hotmail.com>
2016-11-29 09:58:15 +0000

    handling of the IE9 issue
    Presents buttons to DL firefox or chrome in appropriate colours.

--- app/chart.js
+++ app/chart.js
@@ -370,6 +370,7 @@ var cht = {
           //find min in order to set the floor of the chart - to be implemented if desired
           var minHeight = 0;
           var local = true;
+
           var bChart = $('#' + element).highcharts({
             chart: {
               type: 'column',
