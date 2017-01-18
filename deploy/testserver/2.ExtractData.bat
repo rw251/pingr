24ebaf12cb11eef20b19d9fca03610dba9cde654
Richard Williams <richard.williams2@manchester.ac.uk>
2017-01-18 20:48:30 +0000

    sql changes

--- deploy/testserver/2.ExtractData.bat
+++ deploy/testserver/2.ExtractData.bat
@@ -10,7 +10,7 @@ if not exist "%REPORT.DIRECTORY%" mkdir %REPORT.DIRECTORY%
 REM DO EXTRACT
 bcp "SELECT * FROM [%DB%].[dbo].[output.pingr.contacts]" queryout %REPORT.DIRECTORY%/contacts.dat -c -T -b 10000000
 bcp "SELECT * FROM [%DB%].[dbo].[output.pingr.demographics]" queryout %REPORT.DIRECTORY%/demographics.dat -c -T -b 10000000
-bcp "SELECT * FROM [%DB%].[dbo].[output.pingr.denominators]" queryout %REPORT.DIRECTORY%/denominators.dat -c -T -b 10000000
+bcp "SELECT PatID,indicatorId,replace (replace (why, char(10), ''), char(13), '') FROM [%DB%].[dbo].[output.pingr.denominators]" queryout %REPORT.DIRECTORY%/denominators.dat -c -T -b 10000000
 bcp "SELECT * FROM [%DB%].[dbo].[output.pingr.diagnoses]" queryout %REPORT.DIRECTORY%/diagnoses.dat -c -T -b 10000000
 bcp "SELECT * FROM [%DB%].[dbo].[output.pingr.impCodes]" queryout %REPORT.DIRECTORY%/impCodes.dat -c -T -b 10000000
 bcp "SELECT * FROM [%DB%].[dbo].[output.pingr.patActions]" queryout %REPORT.DIRECTORY%/patActions.dat -c -T -b 10000000
