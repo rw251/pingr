ac3793f1e4196ef01a76c1bed94e36ab8a3f2fd2
Richard Williams <richard.williams2@manchester.ac.uk>
2017-02-01 22:41:07 +0000

    wrong indicator display bug fixed?

--- app/views/indicator.js
+++ app/views/indicator.js
@@ -63,6 +63,8 @@ var ind = {
       base.updateTitle(data.text.pathways[pathwayId][pathwayStage].standards[standard].name);
       lookup.suggestionModalText="Screen: Indicator\nIndicator: " + data.text.pathways[pathwayId][pathwayStage].standards[standard].name + "\n===========\n";
 
+      var nothingChanged = layout.pathwayId === pathwayId && layout.pathwayStage === pathwayStage && layout.standard === standard;
+
       layout.pathwayId = pathwayId;
       layout.pathwayStage = pathwayStage;
       layout.standard = standard;
@@ -78,7 +80,10 @@ var ind = {
       base.updateTab("indicators", data.text.pathways[pathwayId][pathwayStage].standards[standard].tabText, [pathwayId, pathwayStage, standard].join("/"));
 
       //check state cache (stateMaintainance)
-      if($('#stateM-indicator').children().length > 0)
+      //RW - added the nothingChanged variable so that this state management
+      // is only invoked if the pathway/stage/standard are the same
+      // Was leading to a bug where the wrong indicator data was displayed
+      if(nothingChanged && $('#stateM-indicator').children().length > 0)
       {
         var indicatorCachedState = $('#stateM-indicator').children();
         base.savePanelState();
@@ -86,7 +91,8 @@ var ind = {
       }
 
       //if not presently loaded
-      if($('#mainPage-tabs').length < 1)
+      //RW - added the not selector to prevent a blank page appearing in some edge cases
+      if($('#mainPage-tabs').not('#stateM-indicator #mainPage-tabs').length < 1)
       {
 
         var tabList = $('<ul id="mainPage-tabs" class="nav nav-tabs"></ul>');
