08c9fcc579c9bd0b54950e8a9139b08a84f6dcb0
Richard Williams <richard.williams2@manchester.ac.uk>
2017-02-08 14:58:08 +0000

    Latest changes

--- deploy/appserver/CheckForPushedChanges.bat
+++ deploy/appserver/CheckForPushedChanges.bat
@@ -1,4 +1,11 @@
 echo off
+REM move to batch directory
+cd /d %~dp0
+
+REM Then move to root of application
+cd ../..
+
+git fetch
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse HEAD`) DO (
 SET LOCAL=%%F
@@ -8,8 +15,13 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse @{u}`) DO (
 SET REMOTE=%%F
 )
 
-IF "%LOCAL%"=="%REMOTE%" (
+IF NOT "%LOCAL%"=="%REMOTE%" (
 	ECHO SAME
+	git pull origin dev
+	
+	npm run build
+	
+	REM Find process id and restart
+	
+	
 )
-
-ECHO DIFF
