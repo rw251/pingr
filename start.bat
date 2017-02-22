4388840454747bb5824c9a9ae76a4fc988a0f972
Richard Williams <richard.williams2@manchester.ac.uk>
2017-02-22 22:21:14 +0000

    change to use forever - don't need forever win anymore

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 cd /d %~dp0
-node .\node_modules\forever\bin\forever --append -o E:\logs\pingr_node_output.txt -e E:\logs\pingr_node_error.txt -l E:\logs\pingr_node_forever.txt start server.js 1> E:\logs\pingr_node_startup.txt 2>&1
+forever --append -o E:\logs\pingr_node_output.txt -e E:\logs\pingr_node_error.txt -l E:\logs\pingr_node_forever.txt start server.js 1> E:\logs\pingr_node_startup.txt 2>&1
