b132ba2acded738216641ca1d9f117566d8579e2
Richard Williams <richard.williams2@manchester.ac.uk>
2017-04-05 16:15:54 +0100

    allow linked actions to be seen from outcome indicators

--- server/controllers/indicators.js
+++ server/controllers/indicators.js
@@ -3,23 +3,23 @@ var Indicator = require('../models/indicator'),
 
 var mergeActions = function(actions, indicators, indicatorId) {
   var actionObject = {};
-  var userDefinedActions=[];
+  var userDefinedActions = [];
   actions.forEach(function(v) {
-    if(v.userDefined && (!indicatorId || v.indicatorList.indexOf(indicatorId))) userDefinedActions.push(v);
+    if (v.userDefined && (!indicatorId || v.indicatorList.indexOf(indicatorId))) userDefinedActions.push(v);
     actionObject[v.actionTextId] = v.toObject();
   });
 
   var uniqueActions = {};
 
-  indicators.forEach(function(indicator){
+  indicators.forEach(function(indicator) {
     //de dupe and sum the pointsPerAction
-    indicator.actions.forEach(function(v){
+    indicator.actions.forEach(function(v) {
       v = v.toObject ? v.toObject() : v;
-      var actionIdFromText = v.actionText.toLowerCase().replace(/[^a-z0-9]/g,"");
+      var actionIdFromText = v.actionText.toLowerCase().replace(/[^a-z0-9]/g, "");
       v.pointsPerAction = +v.pointsPerAction;
       v.indicatorList = [v.indicatorId];
       v.actionTextId = actionIdFromText;
-      if(!uniqueActions[actionIdFromText]) {
+      if (!uniqueActions[actionIdFromText]) {
         uniqueActions[actionIdFromText] = v;
       } else {
         uniqueActions[actionIdFromText].indicatorList.push(v.indicatorId);
@@ -30,15 +30,15 @@ var mergeActions = function(actions, indicators, indicatorId) {
   });
 
   //convert back to array and sort
-  var rtn = Object.keys(uniqueActions).map(function(v){
+  var rtn = Object.keys(uniqueActions).map(function(v) {
     return uniqueActions[v];
   });
 
   //do the merging
   rtn = rtn.map(function(v) {
-    if(actionObject[v.actionTextId]){
-      Object.keys(actionObject[v.actionTextId]).forEach(function(vv){
-        if(vv[0]==="_" || vv === "indicatorList") return; //ignore hidden properties like _id and __v;
+    if (actionObject[v.actionTextId]) {
+      Object.keys(actionObject[v.actionTextId]).forEach(function(vv) {
+        if (vv[0] === "_" || vv === "indicatorList") return; //ignore hidden properties like _id and __v;
         v[vv] = actionObject[v.actionTextId][vv];
       });
     }
@@ -46,21 +46,21 @@ var mergeActions = function(actions, indicators, indicatorId) {
   });
 
   //do the sorting
-  rtn.sort(function(a,b){
-    if(a.agree===false){
-      if(b.agree===false) return b.pointsPerAction - a.pointsPerAction;
+  rtn.sort(function(a, b) {
+    if (a.agree === false) {
+      if (b.agree === false) return b.pointsPerAction - a.pointsPerAction;
       else return 1;
-    } else if (b.agree === false){
+    } else if (b.agree === false) {
       return -1;
-    } else if(a.agree){
-      if(!b.agree) return 1;
-    } else if(b.agree){
+    } else if (a.agree) {
+      if (!b.agree) return 1;
+    } else if (b.agree) {
       return -1;
     }
     return b.pointsPerAction - a.pointsPerAction;
   });
 
-  return {actions: rtn, userDefinedActions: userDefinedActions};
+  return { actions: rtn, userDefinedActions: userDefinedActions };
 };
 
 module.exports = {
@@ -77,7 +77,7 @@ module.exports = {
         return done(null, false);
       } else {
         var opps = [];
-        indicators.forEach(function(v){
+        indicators.forEach(function(v) {
           opps = opps.concat(v.opportunities);
         });
         var patientList = opps.reduce(function(prev, curr) {
@@ -87,30 +87,30 @@ module.exports = {
           });
         }, []);
 
-        actions.patientsWithPlan(patientList, function(err, patientsWithActions){
+        actions.patientsWithPlan(patientList, function(err, patientsWithActions) {
           var indicatorCountOfReviewedPatients = {};
-          patientsWithActions.forEach(function(v){
+          patientsWithActions.forEach(function(v) {
             var localReviewedPatients = {};
-            v.actions.forEach(function(vv){
-              if(vv.indicatorList) {
-                vv.indicatorList.forEach(function(vvv){
-                  if(!localReviewedPatients[vvv]) {
+            v.actions.forEach(function(vv) {
+              if (vv.indicatorList) {
+                vv.indicatorList.forEach(function(vvv) {
+                  if (!localReviewedPatients[vvv]) {
                     localReviewedPatients[vvv] = 1;
                   }
                 });
               }
             });
-            Object.keys(localReviewedPatients).forEach(function(v){
-              if(!indicatorCountOfReviewedPatients[v]) {
+            Object.keys(localReviewedPatients).forEach(function(v) {
+              if (!indicatorCountOfReviewedPatients[v]) {
                 indicatorCountOfReviewedPatients[v] = 1;
               } else {
                 indicatorCountOfReviewedPatients[v]++;
               }
             });
           });
-          indicators = indicators.map(function(v){
+          indicators = indicators.map(function(v) {
             v = v.toObject();
-            if(indicatorCountOfReviewedPatients[v.id]) {
+            if (indicatorCountOfReviewedPatients[v.id]) {
               v.reviewed = indicatorCountOfReviewedPatients[v.id];
             } else {
               v.reviewed = 0;
@@ -141,14 +141,14 @@ module.exports = {
   },
 
   getSpecificActions: function(practiceId, actions, done) {
-    if(!actions || actions.length === 0) return done(null, {});
+    if (!actions || actions.length === 0) return done(null, {});
     var aggQuery = [
-      {$match: {practiceId: practiceId}}, //filter to just practice of interest
-      {$project: {_id: 0, actions: 1}}, //get rid of all fields except patient id and action list
-      {$unwind: "$actions"}, //so we have one object per patient/action combination
-      {$match: {"actions.actionTextId" : {$in :actions.map(function(v){return v.actionTextId;})}}},
-      {$group: {_id: "0", actions: {$push: "$actions"}}}, //regroup the actions into a list
-      {$project: {_id:0, patientId: "$_id", actions: 1}} //rename to original format
+      { $match: { practiceId: practiceId } }, //filter to just practice of interest
+      { $project: { _id: 0, actions: 1 } }, //get rid of all fields except patient id and action list
+      { $unwind: "$actions" }, //so we have one object per patient/action combination
+      { $match: { "actions.actionTextId": { $in: actions.map(function(v) { return v.actionTextId; }) } } },
+      { $group: { _id: "0", actions: { $push: "$actions" } } }, //regroup the actions into a list
+      { $project: { _id: 0, patientId: "$_id", actions: 1 } } //rename to original format
     ];
     Indicator.aggregate(aggQuery, function(err, indicators) {
       if (err) {
@@ -159,7 +159,7 @@ module.exports = {
         console.log('Invalid request for patientId: ' + patientId);
         return done(null, false);
       } else {
-        var rtn = mergeActions(actions, indicators)          ;
+        var rtn = mergeActions(actions, indicators);
         return done(null, rtn);
       }
     });
@@ -168,22 +168,42 @@ module.exports = {
   //Get team actions for a single indicator or all indicators
   getActions: function(practiceId, indicatorId, done) {
     var searchObject = { practiceId: practiceId };
-    actions.getTeam({ practiceId: practiceId, patientId: {$exists: false} }, function(err, actions) {
+    actions.getTeam({ practiceId: practiceId, patientId: { $exists: false } }, function(err, actions) {
       if (err) return done(err);
       if (indicatorId) {
         searchObject.id = indicatorId;
       }
-      Indicator.find(searchObject, { _id: 0, actions: 1 }, function(err, indicators) {
+      Indicator.find(searchObject, { _id: 0, actions: 1, mappedIndicators: 1, type: 1 }, function(err, indicators) {
         if (err) {
           console.log(err);
           return done(new Error("Error finding indicator"));
         }
-        if (!indicators) {
-          console.log('Invalid request for indicatorId: ' + indicatorId);
-          return done(null, false);
+
+        if (indicatorId && indicators[0].type === "outcome") {
+          //we need to find the actions for the associated process indicators
+
+          searchObject.id = { $in: indicators[0].mappedIndicators };
+          Indicator.find(searchObject, { _id: 0, actions: 1, mappedIndicators: 1, type: 1 }, function(err, indicators) {
+            if (err) {
+              console.log(err);
+              return done(new Error("Error finding indicator"));
+            }
+            if (!indicators) {
+              console.log('Invalid request for indicatorId: ' + indicatorId);
+              return done(null, false);
+            } else {
+              var rtn = mergeActions(actions, indicators, indicatorId);
+              return done(null, rtn);
+            }
+          });
         } else {
-          var rtn = mergeActions(actions, indicators, indicatorId);
-          return done(null, rtn);
+          if (!indicators) {
+            console.log('Invalid request for indicatorId: ' + indicatorId);
+            return done(null, false);
+          } else {
+            var rtn = mergeActions(actions, indicators, indicatorId);
+            return done(null, rtn);
+          }
         }
       });
     });
@@ -192,8 +212,8 @@ module.exports = {
   //Get benchmark data for an indicator
   getBenchmark: function(practiceId, practices, indicatorId, done) {
     var pLookup = {};
-    practices.forEach(function(v){
-      pLookup[v._id]=[v.name,v.neighbourhood];
+    practices.forEach(function(v) {
+      pLookup[v._id] = [v.name, v.neighbourhood];
     });
     Indicator.find({ id: indicatorId }, function(err, indicators) {
       if (err) {
@@ -204,14 +224,14 @@ module.exports = {
         console.log('Error finding indicators for:  ' + indicatorId);
         return done(null, false);
       } else {
-        var benchmark = indicators.filter(function(v){
-          return v.values && v.values.length>0 && v.practiceId !== "ALL";
+        var benchmark = indicators.filter(function(v) {
+          return v.values && v.values.length > 0 && v.practiceId !== "ALL";
         }).map(function(v) {
           return {
-            x: +v.values[1][v.values[1].length - 1]*100/+v.values[2][v.values[2].length - 1],
+            x: +v.values[1][v.values[1].length - 1] * 100 / +v.values[2][v.values[2].length - 1],
             p: v.practiceId === practiceId ? "You" : practiceId,
             pFull: v.practiceId === practiceId ? "You" : (pLookup[v.practiceId] ? pLookup[v.practiceId][0] : v.practiceId),
-            local: pLookup[v.practiceId] ? pLookup[v.practiceId][1]===pLookup[practiceId][1] : false
+            local: pLookup[v.practiceId] ? pLookup[v.practiceId][1] === pLookup[practiceId][1] : false
           };
         });
         done(null, benchmark);
