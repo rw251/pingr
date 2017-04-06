2816aa35c6a04c0479582177e0c9c553859e0490
Richard Williams <richard.williams2@manchester.ac.uk>
2017-04-06 15:08:13 +0100

    Changed scheduled task to run every hour for email reminders

--- deploy/appserver/Setup_scheduled_tasks.bat
+++ deploy/appserver/Setup_scheduled_tasks.bat
@@ -7,6 +7,6 @@ SET START.BAT=D:\pingr\start.bat
 SET BACKUP.BAT=D:\pingr\deploy\appserver\mongobackup.bat
 
 SchTasks /RU system /Create /SC DAILY /TN "PINGR Data Loader" /TR "\"%BATCH.DIRECTORY%RunPINGRLoaderScript_ST_DEBUG.bat\"" /RI 20 /ST 00:12 /DU 23:00
-SchTasks /RU system /Create /SC DAILY /TN "PINGR Email Reminder" /TR "\"%BATCH.DIRECTORY%RunEmailReminderScript_ST_DEBUG.bat\"" /ST 07:03
+SchTasks /RU system /Create /SC DAILY /TN "PINGR Email Reminder" /TR "\"%BATCH.DIRECTORY%RunEmailReminderScript_ST_DEBUG.bat\"" /RI 60 /ST 04:03 /DU 23:00
 SchTasks /RU system /Create /SC ONSTART /TN "PINGR web app" /TR "\"%START.BAT%\""
 SchTasks /RU system /Create /SC DAILY /TN "PINGR MongoDB backup" /TR "\"%BACKUP.BAT%\"" /ST 21:10
