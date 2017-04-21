698e4e8d20f59976ffaa6ce6a16d61923a4df8cd
Richard Williams <richard.williams2@manchester.ac.uk>
2017-04-21 09:04:36 +0100

    Saved message is back

--- app/notify.js
+++ app/notify.js
@@ -1,9 +1,16 @@
+var messages = ["Thanks!", "Cheers!", "Nice one!", "Nice work!", "Great!"];
+
+var randomMessage = function () {
+  return messages[Math.floor(Math.random() * messages.length)];
+};
+
 module.exports = {
 
-  showSaved: function() {
+  showSaved: function () {
+    $("#saved-message h1").text("Saved! " + randomMessage());
     $("#saved-message").hide().toggleClass("in").fadeIn(300);
-    window.setTimeout(function() {
-      $("#saved-message").fadeOut(300, function() {
+    window.setTimeout(function () {
+      $("#saved-message").fadeOut(300, function () {
         $(this).toggleClass("in");
       });
     }, 2000);
