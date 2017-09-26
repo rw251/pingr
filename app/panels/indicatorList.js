f8bd97e6aa711f5ca224a1f7fae2c128a9b0a228
Richard Williams <richard.williams2@manchester.ac.uk>
2017-09-26 11:03:40 +0100

    Data tab;es on indicator screen

--- app/panels/indicatorList.js
+++ app/panels/indicatorList.js
@@ -4,6 +4,17 @@ var base = require('../base.js'),
   state = require('../state'),
   sparkline = require('jquery-sparkline');
 
+var tableProcessIndicators;
+var tableOutcomeIndicators;
+var format = function (description) {
+  // `d` is the original data object for the row
+  return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+      '<tr>'+
+          '<td>' + description + '</td>'+
+      '</tr>'+
+  '</table>';
+}
+
 var indicatorList = {
 
   show: function(panel, isAppend, loadContentFn) {
@@ -53,11 +64,93 @@ var indicatorList = {
         width: "100px"
       });
 
-      $('#overview-table-process, #overview-table-outcomes').floatThead({
-        position: 'absolute',
-        scrollContainer: true,
-        zIndex:50
+      tableOutcomeIndicators = $('#overview-table-outcomes').DataTable({
+        searching: true, //we want a search box
+        stateSave: true, // let's remember which page/sorting etc
+        //dom: '<"row"<"col-sm-7 toolbar"i><"col-sm-5"B>>rt<"row"<"col-sm-5"l><"col-sm-7"p>><"clear">',
+        /*columnDefs: list["header-items"].map(function (v, i) {
+          var thing = { type: v.type, orderSequence: v.orderSequence, targets: i, name:"shown" };
+          if(v.hidden) {
+            thing.visible=false;
+            thing.name="not";
+          } else if(i<numColumns-2){
+            //everything sorted by exclusion first except exclusion column
+            thing.orderData = [[numColumns-1,"asc"], i];
+          }
+          return thing;
+        }),*/
+        scrollY: '50vh',
+        scrollCollapse: true,
+        // buttons: [
+        //   {
+        //     extend: 'colvis',
+        //     columns: 'shown:name'
+        //   },
+        //   {
+        //     text: 'Pdf',
+        //     className: 'download-button',
+        //     action: function (e, dt, node, config) {
+        //       //writePdf();
+        //     }
+        //   }
+        // ],
+        // infoCallback: function (settings, start, end, max, total, pre) {
+        //   return "showing " + start + " to " + end + " of " + total + " patients" + (list.numExcluded>0 ? " [including " + list.numExcluded + " exclusions]" : "");
+        // }
       });
+
+      tableProcessIndicators = $('#overview-table-process').DataTable({
+        searching: true, //we want a search box
+        stateSave: true, // let's remember which page/sorting etc
+        //dom: '<"row"<"col-sm-7 toolbar"i><"col-sm-5"B>>rt<"row"<"col-sm-5"l><"col-sm-7"p>><"clear">',
+        columnDefs: [
+          { "width": "90px", "targets": 6 }
+        ],
+        order: [[ 1, "desc" ]],
+        /*columnDefs: list["header-items"].map(function (v, i) {
+          var thing = { type: v.type, orderSequence: v.orderSequence, targets: i, name:"shown" };
+          if(v.hidden) {
+            thing.visible=false;
+            thing.name="not";
+          } else if(i<numColumns-2){
+            //everything sorted by exclusion first except exclusion column
+            thing.orderData = [[numColumns-1,"asc"], i];
+          }
+          return thing;
+        }),*/
+        scrollY: '50vh',
+        scrollCollapse: true,
+        // buttons: [
+        //   {
+        //     extend: 'colvis',
+        //     columns: 'shown:name'
+        //   },
+        //   {
+        //     text: 'Pdf',
+        //     className: 'download-button',
+        //     action: function (e, dt, node, config) {
+        //       //writePdf();
+        //     }
+        //   }
+        // ],
+        // infoCallback: function (settings, start, end, max, total, pre) {
+        //   return "showing " + start + " to " + end + " of " + total + " patients" + (list.numExcluded>0 ? " [including " + list.numExcluded + " exclusions]" : "");
+        // }
+      });
+
+      // $('#overview-table-process, #overview-table-outcomes').floatThead({
+      //   position: 'absolute',
+      //   scrollContainer: true,
+      //   zIndex:50
+      // });
+
+      setTimeout(function () {
+        tableProcessIndicators.columns.adjust().draw(false);
+        tableOutcomeIndicators.columns.adjust().draw(false);
+
+        base.setupClipboard('.btn-copy', true);
+        base.wireUpTooltips();
+      }, 100);
 
       indicatorList.wireUp(panel, loadContentFn);
 
@@ -96,19 +189,42 @@ var indicatorList = {
       $('.show-more-row[data-id="' + id + '"],.standard-row[data-id="' + id + '"]').removeClass('hover');
     });
 
-    panel.off('click', '.show-more');
-    panel.on('click', '.show-more', function(e) {
+    panel.off('click', '#overview-table-process .show-more');
+    panel.on('click', '#overview-table-process .show-more', function(e) {
       var id = $(this).data("id");
-      var elem = $('.show-more-row[data-id="' + id + '"]');
-      if(elem.is(':visible')){
-        $('.show-more[data-id="' + id + '"]:first').show();
-        elem.hide();
-      } else {
-        $('.show-more-row').hide();
-        $('.show-more').show();
-        $(this).hide();
-        elem.show('fast');
-        log.event("show-more", window.location.hash, [{key:"indicator",value:id}]);
+      var tr = $(this).closest("tr");
+      var row = tableProcessIndicators.row( tr );
+      if ( row.child.isShown() ) {
+        // This row is already open - close it
+        row.child.hide();
+        tr.removeClass('shown');
+      }
+      else {
+        // Open this row
+        row.child( format(tr.data('description')) ).show();
+        tr.addClass('shown');
+      }
+      // do not give a default action
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
+    });
+
+
+    panel.off('click', '#overview-table-outcome .show-more');
+    panel.on('click', '#overview-table-outcome .show-more', function(e) {
+      var id = $(this).data("id");
+      var tr = $(this).closest("tr");
+      var row = tableOutcomeIndicators.row( tr );
+      if ( row.child.isShown() ) {
+        // This row is already open - close it
+        row.child.hide();
+        tr.removeClass('shown');
+      }
+      else {
+        // Open this row
+        row.child( format(tr.data('description')) ).show();
+        tr.addClass('shown');
       }
       // do not give a default action
       e.preventDefault();
@@ -118,9 +234,25 @@ var indicatorList = {
 
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
       $.sparkline_display_visible(); //ensure sparklines on hidden tabs display
+      // setTimeout(function(){
+      tableProcessIndicators.columns.adjust().draw("page"); //ensure column headers are correct
+      // },200);
+      tableOutcomeIndicators.columns.adjust().draw(false); //ensure column headers are correct
       //e.target // newly activated tab
       //e.relatedTarget // previous active tab
-    })
+    });
+
+    $('#overview-table-outcomes').on('draw.dt', function () {
+      $.sparkline_display_visible(); //ensure sparklines on hidden tabs display
+      //tableOutcomeIndicators.columns.adjust().draw(false); //ensure column headers are correct
+      console.log('Redraw occurred at: ' + new Date().getTime());
+    });
+    
+    $('#overview-table-process').on('draw.dt', function () {
+      $.sparkline_display_visible(); //ensure sparklines on hidden tabs display
+      //tableProcessIndicators.columns.adjust().draw(false); //ensure column headers are correct
+      console.log('Redraw occurred at: ' + new Date().getTime());
+    });
 
     $('#overview-table-process, #overview-table-outcomes').floatThead('reflow')
   }
