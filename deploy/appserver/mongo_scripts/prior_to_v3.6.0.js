b164b47650ffaeddc5f0ccbee50cb1e0566e21d2
Richard Williams <richard.williams2@manchester.ac.uk>
2017-09-27 17:01:11 +0100

    Mongo update script added

---
+++ deploy/appserver/mongo_scripts/prior_to_v3.6.0.js
@@ -0,0 +1,17 @@
+// execute the following prior to v3.6.0 to move practice info into an array in the user collection
+
+db.users.find().snapshot().forEach(
+  function (e) {
+    // update document, using its own properties
+    e.practices = [{ id: e.practiceId, name: e.practiceName, authorised: true }];
+
+    // remove old properties
+    delete e.practiceId;
+    delete e.practiceName;
+    delete e.practiceIdNotAuthorised;
+    delete e.practiceNameNotAuthorised;
+
+    // save the updated document
+    db.users.save(e);
+  }
+)
