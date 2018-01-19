b423d8f5a14e59f654af6881a9d79f0a759c4bd5
Richard Williams <richard.williams2@manchester.ac.uk>
2018-01-19 13:16:50 +0000

    Added some packages for tests

---
+++ test/app/log.test.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const sinon = require('sinon');
+require('jsdom-global')();
+const log = require('../../app/log');
+
+let xhr;
+let requests;
+
+describe('log', () => {
+  beforeEach(() => {
+    xhr = sinon.useFakeXMLHttpRequest();
+    requests = [];
+    xhr.onCreate = function pushRequest(req) { requests.push(req); };
+  });
+
+  afterEach(() => {
+    xhr.restore();
+  });
+
+  it('should send navigate event', () => {
+    const toUrl = 'http://test';
+    const dataProp = { some: 'props', keys: [1, 2, 3] };
+
+    log.navigate(toUrl, dataProp);
+
+    assert.equals(requests.length = 1);
+    assert.match(requests[0].url, '/todo/42/items');
+  });
+
+  // We will place our tests cases here
+});
