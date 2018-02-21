1656704c97b6a2a7e13e89b4a66671cd445fa26f
Richard Williams <richard.williams2@manchester.ac.uk>
2018-02-21 10:08:42 +0000

    Added arrow to tutorial

--- app/sideshow.js
+++ app/sideshow.js
@@ -21,41 +21,48 @@ module.exports = () => {
   }).storyLine({
     showStepPosition: true,
     steps: [
-      {
-		    title: "The PINGR tutorial",
-		    text: "OK! Let's show you how to use PINGR."
-		  },
-      {
-		    title: "Here to help",
-		    text: "PINGR is here to suggest actions to improve patient care. All of it's suggestions are based on your practice's data."
-      },
-      {
-        title: "You can help PINGR",
-        text: "Your input improves PINGR over time. By giving a thumb_down you help PINGR's suggestions for all users"
-      },
+      // {
+		  //   title: "The PINGR tutorial",
+		  //   text: "OK! Let's show you how to use PINGR."
+		  // },
+      // {
+		  //   title: "Here to help",
+		  //   text: "PINGR is here to suggest actions to improve patient care. All of it's suggestions are based on your practice's data."
+      // },
+      // {
+      //   title: "You can help PINGR",
+      //   text: "Your input improves PINGR over time. By giving a thumb_down you help PINGR's suggestions for all users"
+      // },
       {
         title: "System Buttons",
         text: "These buttons are system level buttons. They are used to let you log out, change your password or contact the admins to help you out!",
         lockSubject: true,
-        subject: ".nav.navbar-nav.navbar-right"
+        subject: ".nav.navbar-nav.navbar-right",
+        targets: ".nav.navbar-nav.navbar-right",
+        arrowPosition: "bottom"
       },
       {
         title: "Navigation",
         text: "With these buttons you can access other panels in the system. In general you will move from left (overview panel) to right (action plans panel).",
         lockSubject: true,
-        subject: "#mainTab"
+        subject: "#mainTab",
+        targets: "#mainTab",
+        arrowPosition: "bottom"
       },
       {
         title: "Navigation",
         text: "Press these buttons to take you to a specific part of the system in case you want to go somewhere specific.",
         lockSubject: true,
-        subject: "#mainTab"
+        subject: "#mainTab",
+        targets: "#mainTab",
+        arrowPosition: "bottom"
       },
       {
         title: "The log out button.",
         text: "Click this button if you want to log out of the system.",
         subject: ".nav.navbar-nav.navbar-right",
-        targets: "#suggs"
+        targets: "#suggs",
+        arrowPosition: "bottom"
       }
     ]
   });
